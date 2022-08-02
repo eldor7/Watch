f9e7b6bc4060cbb30bb46d6a62b2ae8663f456d0
Eldorbek <eldor777bek@naver.com>
2022-08-02 12:53:42 +0900

    pro

---
+++ script.js
@@ -0,0 +1,42 @@
+setInterval(setAsosiy,1000)
+
+const milSoat = document.querySelector('[bu-soat-mili]')
+const milMinut = document.querySelector('[bu-minut-mili]')
+const milsekund = document.querySelector('[bu-sekund-mili]')
+
+function setAsosiy() {
+    const currentDate = new Date()
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 
+            60
+       const hoursRatio = (minutesRatio + currentRatio.getHours()) / 12
+       setRotation(milsekund, secondsRatio)
+       setRotation(milMinut, minutesRatio)
+       setRotation(milSoat, hoursRatio)
+
+}
+
+     function setRotation(element, rotationRatio) {
+         element.style.setProperty('--rotation', rotationRatio * 360)
+     }
+    
+
+setAsosiy()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
